7594bc3e38c315426cf1b979595cf16a9dc0ef5b
Fito Toledano <fito@peepteam.com>
2020-12-10 00:19:58 +0100

    Added AccountViewForm Toggles

--- Views/Account/AccountView.swift
+++ Views/Account/AccountView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var birthday: Date = Date()
+    @State private var extraNapkins: Bool = false
+    @State private var frequentRefills: Bool = false
+    
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle(Text("Account"))
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .keyboardType(.emailAddress)
+                    
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                }
+                
+                Section (header: Text("Requests")) {
+                    Toggle("Extra napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+                
+            }
+            .navigationTitle(Text("Account"))
         }
     }
 }
